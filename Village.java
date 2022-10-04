ca03ec0ef4ade8097c04cff8cfd817c8f66c064c
VeronikaRadionova <114427643+VeronikaRadionova@users.noreply.github.com>
2022-10-05 00:21:23 +0200

    Add files via upload

---
+++ Village.java
@@ -0,0 +1,14 @@
+package personnages;
+
+public class Village {
+	private String nom;
+	public Village(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+		
+}
